2a0edbd61460c5868b281ae0132156b4ba08a4ba
Ivan Silva <silvaivan280@gmail.com>
2024-12-13 16:32:10 -0300

    limpie el proyecto principal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './components/common/Menu';
+
+function App() {
+  
+
+  return (
+    <>
+     <Menu/>
+    </>
+  )
+}
+
+export default App
